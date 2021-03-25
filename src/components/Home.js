6a95eb4f4adfd55ed90a252e8da63d1ea5efe70a
Falua Temitope Oyewole <emmat0616@gmail.com>
2021-03-25 03:03:08 +0100

    Building Product

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../assets/css/Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -8,6 +9,14 @@ function Home() {
         className="home__image"
         src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
         alt="banner"
+      />
+
+      <Product
+        id="121213"
+        title="The Lean Startup: How Constant Innovation Creative"
+        price={11.96}
+        rating={5}
+        images="https://images-eu.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
       />
     </div>
   );
