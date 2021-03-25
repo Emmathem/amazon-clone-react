40e1e2f135560da7e37309d805f9f3fd87aedbd1
Falua Temitope Oyewole <emmat0616@gmail.com>
2021-03-25 02:27:51 +0100

    Banner done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './Header';
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -9,13 +10,16 @@ function App() {
       <div className="App">
         <Switch>
           <Route path='/checkout'>
+            <Header />
             <h1>Checkout</h1>
           </Route>
           <Route path='/login'>
+            <Header />
             <h1>Login Page</h1>
           </Route>
           <Route path='/'>
             <Header />
+            <Home />
             <h1>Welcome to Amazon</h1>
           </Route>
         </Switch>
